e3ffe3b080fad580b0114ac0755adc9543df7ec7
Summon <@SUMMON>
2019-05-14 21:11:24 +0800

    滕东昊改

---
+++ MedicalClinicKHD/Controllers/NurseController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedicalClinicKHD.Controllers
+{
+    public class NurseController : Controller
+    {
+        // GET: Nurse
+        /// <summary>
+        /// 护士登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NurseLogin()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 病人挂号
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Registered()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 病人注册
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PatientAdd()
+        {
+            return View();
+        }
+    }
+}
