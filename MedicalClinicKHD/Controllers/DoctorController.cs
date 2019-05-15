6721d60adee5778ac8b8f0f3e8b53b1702967866
顺 齐 <2490475248@qq.com>
2019-05-15 14:21:13 +0800

    AddModelas

--- MedicalClinicKHD/Controllers/DoctorController.cs
+++ MedicalClinicKHD/Controllers/DoctorController.cs
@@ -8,6 +8,7 @@ namespace MedicalClinicKHD.Controllers
 {
     public class DoctorController : Controller
     {
+        static HttpCookie kc = null;
         // GET: Doctor
         public ActionResult Index()
         {
@@ -22,12 +23,21 @@ namespace MedicalClinicKHD.Controllers
             var i = Hctp.GetApi("get", "Doctor/LoginDoctor?name=" + title + "&pwd=" + password);
             if (Convert.ToInt32(i) > 0)
             {
+                kc = new HttpCookie("name",title);
                 return Content("成功");
             }
             else
             {
                 return Content("失败");
             }
+        }
+        public ActionResult Registration()
+        {
+            var i = kc.Value;
+            var getDocId = Hctp.GetApi("get", "Doctor/GetLoginTable");
+
+            var list = 
+            return View();
         }
     }
 }
