47311a60c5bb9fc94a38e6762eab8736f7bd0be5
admin <admin@DESKTOP-FQOF9AC>
2019-05-15 21:22:33 +0800

    4dsa

---
+++ MedicalClinicKHD/Models/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicalClinicKHD.Models
+{
+    public class Doctor
+    {
+        public int Doc_Id { get; set; } //  医生表Id
+        public int Adm_Id { get; set; } //  医生所属科室
+        public string Doc_Name { get; set; } // 医生姓名
+        public int Doc_Age { get; set; } // 医生年龄
+        public string Doc_Sex { get; set; } // 医生性别
+        public string Doc_Time { get; set; } //  入职时间
+        public int Doc_Type { get; set; } // 医生状态
+        public int Sl_Id { get; set; }//用户登录Id
+        public string Adm_Name { get; set; }///可是名称
+    }
+}
