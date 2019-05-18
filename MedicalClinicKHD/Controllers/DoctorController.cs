f9fd0538b7a08ec5253361d7a0c48c8fbaabd0fa
顺 齐 <2490475248@qq.com>
2019-05-18 08:58:51 +0800

    111

--- MedicalClinicKHD/Controllers/DoctorController.cs
+++ MedicalClinicKHD/Controllers/DoctorController.cs
@@ -53,7 +53,7 @@ namespace MedicalClinicKHD.Controllers
             //查询当前医生下的说有挂号信息
             var list1 = JsonConvert.DeserializeObject<List<Registration>>(list).Where(m => m.Doc_Id == getDocId01).ToList();
             //查询当天的就诊信息
-            var list2 = list1.Where(m => Convert.ToDateTime(m.Reg_Time).Day - data.Day == 0).ToList().Where(m => m.Reg_Type == 0 || m.Reg_Type == 1).ToList();
+            var list2 = list1.Where(m => Convert.ToDateTime(m.Reg_Time).Day - data.Day == 0 && Convert.ToDateTime(m.Reg_Time).Month - data.Month==0).ToList().Where(m => m.Reg_Type == 0 || m.Reg_Type == 1).ToList();
             return list2;
         }
         [HttpGet]
@@ -79,26 +79,42 @@ namespace MedicalClinicKHD.Controllers
                 return Content("叫号失败");
             }
         }
-        public ActionResult Type_change2(int id)
-        {
-            
-            var list = GetTs(id);
-            list.Reg_Type = 2;
-            var i = Hctp.GetApi("post", "Doctor/UptRegistrations", list);
-            if (Convert.ToInt32(i) > 0)
-            {
-                return Content("成功");
-            }
-            else
-            {
-                return Content("失败");
-            }
-        }
         public Registration GetTs(int id)
         {
             var list = Hctp.GetApi("get", "Doctor/GetRegistrations");
             var list1 = JsonConvert.DeserializeObject<List<Registration>>(list).Where(m => m.Reg_Id == id).FirstOrDefault();
             return list1;
+        }
+        public ActionResult Doctorecord()
+        {
+            return View();
+        }
+        public ActionResult AddAndChangeZc(Doctorecord d)
+        {
+            var id = d.Pat_Id;
+            var list = GetTs(id);
+            d.Adm_Id = list.Adm_Id;
+            d.Doc_Id = list.Doc_Id;
+            d.Dcr_Time = DateTime.Now.ToString("yyyy-MM-dd");
+            var i = Hctp.GetApi("post", "Doctor/AddGetDoctorecord",d);
+            if (Convert.ToInt32(i) > 0)
+            {
+                var Gt = GetTs(d.Pat_Id);
+                Gt.Reg_Type = 2;
+                var i1 = Hctp.GetApi("post", "Doctor/UptRegistrations", Gt);
+                if (Convert.ToInt32(i) > 0)
+                {
+                    return Content("成功");
+                }
+                else
+                {
+                    return Content("失败");
+                }
+            }
+            else
+            {
+                return Content("添加就诊记录失败");
+            }
         }
     }
 }
